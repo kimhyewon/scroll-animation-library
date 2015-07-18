d1bcd947e4bcfaabf6d9c7e65914ef1398ad3093
GraceLilac <lovelove8926@naver.com>
2015-07-19 02:00:00 +0900

    size control 버튼 생성, 삭제

--- test.js
+++ test.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', function(){
 	var sampleSquare = document.querySelector('.square');
 
 	sampleSquare.addEventListener('mousedown', function(event){
+		var tmp;
 		event.preventDefault();
 		(new shape.Square())
 			.setPosition(event.pageX-30,event.pageY-30)
 			.startDragging(event)
-			.setParent(document.body);
+			.setParent(document.body)
 	});
 });
 
@@ -18,8 +19,8 @@ var shape = (function() {
 		
 		this.img = document.createElement("img");
 		this.img.src = "rose.png";
-		this.img.width = "50";
-		this.img.height = "50";
+		this.img.width = "60";
+		this.img.height = "60";
 		this.element.appendChild(this.img);
 		// <img src="rose.png" height="50" width="50">
 
@@ -39,6 +40,18 @@ var shape = (function() {
 	Square.prototype.startDragging = function(e) {
 		this._onDragStart();
 		this.element.classList.add('dragging');
+
+		this.sizeBtn = document.createElement("img");
+		this.sizeBtn.className = "size_btn";
+		this.sizeBtn.src = "size3.png";
+		this.sizeBtn.style.position = "absolute";
+		this.sizeBtn.width = "28";
+		this.sizeBtn.height = "28";
+
+		this.sizeBtn.style.top = "-12px";
+		this.sizeBtn.style.left = "-12px";
+		this.sizeBtn.style.marginTop = "none";
+		this.element.appendChild(this.sizeBtn);
 
 		return this;
 	};
@@ -63,11 +76,21 @@ var shape = (function() {
 		document.removeEventListener('mousemove', this._onMouseMove);
 		document.removeEventListener('mouseup', this._onMouseUp);
 
+		this.sizeControll();
+
 		e.target.addEventListener('dblclick', this.positionEnd);
+
+
+		// e.target.addEventListener('dblclick', this.startDragging(e));
 	};
+
+	Square.prototype.sizeControll = function(e) {
+
+	}
 
 	Square.prototype.positionEnd = function(e) {
 		e.target.parentNode.classList.remove("dragging");
+		e.target.parentNode.querySelector(".size_btn").remove("size_btn");
 	}
 
 	Square.prototype.setParent = function(parent) {
