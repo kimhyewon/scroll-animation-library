8332225bca148c77199475a36b633e83efb63916
GraceLilac <lovelove8926@naver.com>
2015-07-24 04:52:20 +0900

    rotate 완료

--- test.js
+++ test.js
@@ -35,16 +35,27 @@ var shape = (function() {
 		this.sizeBtn.style.position = "absolute";
 		this.sizeBtn.width = "28";
 		this.sizeBtn.height = "28";
-		this.sizeBtn.style.top = "-12px";
-		this.sizeBtn.style.left = "-12px";
+		this.sizeBtn.style.top = "-13px";
+		this.sizeBtn.style.left = "-13px";
 		this.sizeBtn.style.marginTop = "none";
+
+		// ratate 버튼 생성
+		this.rotateBtn = document.createElement("img");
+		this.rotateBtn.className = "rotate_btn";
+		this.rotateBtn.src = "rotate2.png";
+		this.rotateBtn.style.position = "absolute";
+		this.rotateBtn.width = "28";
+		this.rotateBtn.height = "28";
+		this.rotateBtn.style.top = "-13px";
+		this.rotateBtn.style.left = "48px";
+		this.rotateBtn.style.marginTop = "none";
 		
 
 		this._onMouseMove = this._onMouseMove.bind(this);
 		this._onMouseUp = this._onMouseUp.bind(this);
 
-		// this._onBtnMouseMove = this._onBtnMouseMove.bind(this);
-		// this._onBtnMouseUp = this._onBtnMouseUp.bind(this);
+		this._onBtnMouseMove = this._onBtnMouseMove.bind(this);
+		this._onBtnMouseUp = this._onBtnMouseUp.bind(this);
 	}
 
 	Square.prototype.setPosition = function(e1, e2) {
@@ -60,6 +71,7 @@ var shape = (function() {
 		this.element.classList.add('dragging');
 
 		this.element.appendChild(this.sizeBtn); // 이부분 중요. 다시 클릭했을 때 버튼 나타나도록 
+		this.element.appendChild(this.rotateBtn);
 
 		return this;
 	};
@@ -87,47 +99,80 @@ var shape = (function() {
 		//더블클릭하면 테두리 제거되며 스티커 위치 고정되는 효과
 		e.target.addEventListener('dblclick', this.positionEnd);
 
-		//사이즈 버튼을 클릭하면 크기 조절할 수 있도록 하는 이벤트 호출 
-		// e.target.parentNode.querySelector(".size_btn").addEventListener("mousedown", this.sizeControll.bind(this));
-
 		//스티커를 다시 클릭하면 움직일 수 있도록 드래그 이벤트 실행시켜줌 
 		e.target.addEventListener('mousedown', this.startDragging.bind(this));
+
+		//사이즈 버튼을 클릭하면 크기 조절할 수 있도록 하는 이벤트 호출 
+		e.target.parentNode.querySelector(".size_btn").addEventListener("mousedown", this.sizeControll.bind(this));
+	
+		//rotate 버튼 클릭하면 rotate 이벤트 호출 
+		e.target.parentNode.querySelector(".rotate_btn").addEventListener("mousedown", this.rotateControll.bind(this));
+	
 	};
 
-	// Square.prototype.sizeControll = function(e) {
-	// 	this._startBtnX = event.pageX;
-	// 	this._startBtnY = event.pageY;
-	// 	this._startBtnTop = parseInt(this.element.style.top);
-	// 	this._startBtnLeft = parseInt(this.element.style.left);
+	Square.prototype.rotateControll = function(e) {
+		e.preventDefault();
 
-	// 	this._onBtnMouseMove = this._onBtnMouseMove.bind(this);
-	// 	this._onBtnMouseUp = this._onBtnMouseUp.bind(this);
+      	this._onRotateBtnMouseMove = this._onRotateBtnMouseMove.bind(this);
+		this._onRotateBtnMouseUp = this._onRotateBtnMouseUp.bind(this);
+      	
+      	document.addEventListener('mousemove', this._onRotateBtnMouseMove);
+		document.addEventListener('mouseup', this._onRotateBtnMouseUp);
+	}
 
-	// 	document.addEventListener('mousemove', this._onBtnMouseMove.bind(this));
-	// 	document.addEventListener('mouseup', this._onBtnMouseUp.bind(this));
-	// }
+	Square.prototype._onRotateBtnMouseMove = function(e) {
+		this.mouse_x = event.pageX;
+      	this.mouse_y = event.pageY;
+		this._startRotateBtnTop = parseInt(this.element.style.top);
+		this._startRotateBtnLeft = parseInt(this.element.style.left);
+		this.center_x = this._startRotateBtnLeft + (this.element.offsetWidth / 2);
+      	this.center_y = this._startRotateBtnTop + (this.element.offsetHeight / 2);
+		
+      	this.radians = Math.atan2(this.mouse_x - this.center_x, this.mouse_y - this.center_y);
+      	this.degree = (this.radians * (180 / Math.PI) * -1) + 130;
 
-	// Square.prototype._onBtnMouseMove = function(e) {
-	// 	console.log(e.target.parentNode);
-	// 	var diffX = event.pageX - this._startBtnX, diffY = event.pageY - this._startBtnY;
-	// 	e.target.parentNode.height = this._startBtnTop + diffY;
-	// 	e.target.parentNode.width = this._startBtnLeft + diffX;
-	// 	this.element.querySelector(".rose").height = this._startBtnTop + diffY;
-	// 	this.element.querySelector(".rose").width = this._startBtnLeft + diffX;
-	// };
+		this.element.style.WebkitTransform = 'rotate(' + this.degree + 'deg)';
+	
+	}
 
-	// Square.prototype._onBtnMouseUp = function(e) {
-	// 	this._onBtnMouseMove = this._onBtnMouseMove.bind(this);
-	// 	this._onBtnMouseUp = this._onBtnMouseUp.bind(this);
+	Square.prototype._onRotateBtnMouseUp = function(e) {
+		document.removeEventListener('mousemove', this._onRotateBtnMouseMove);
+		document.removeEventListener('mouseup', this._onRotateBtnMouseUp);
+	}
 
-	// 	document.removeEventListener('mousemove', this._onBtnMouseMove.bind(this));
-	// 	document.removeEventListener('mouseup', this._onBtnMouseUp.bind(this));
-	// };
+	Square.prototype.sizeControll = function(e) {
+		e.preventDefault();
+		this._startBtnX = event.pageX;
+		this._startBtnY = event.pageY;
+		this._startBtnTop = parseInt(this.element.style.top);
+		this._startBtnLeft = parseInt(this.element.style.left);
 
+		this._onBtnMouseMove = this._onBtnMouseMove.bind(this);
+		this._onBtnMouseUp = this._onBtnMouseUp.bind(this);
+
+		document.addEventListener('mousemove', this._onBtnMouseMove);
+		document.addEventListener('mouseup', this._onBtnMouseUp);
+	}
+
+	Square.prototype._onBtnMouseMove = function(e) {
+		var diffX = event.pageX - this._startBtnX, diffY = event.pageY - this._startBtnY;
+		e.target.parentNode.height = this._startBtnTop + diffY;
+		e.target.parentNode.width = this._startBtnLeft + diffX;
+		console.log(this.element.height);
+		this.element.height = this._startBtnTop + diffY;
+		this.element.width = this._startBtnLeft + diffX;
+	};
+
+	Square.prototype._onBtnMouseUp = function(e) {
+
+		document.removeEventListener('mousemove', this._onBtnMouseMove);
+		document.removeEventListener('mouseup', this._onBtnMouseUp);
+	};
 
 	Square.prototype.positionEnd = function(e) {
 		e.target.parentNode.classList.remove("dragging");
 		e.target.parentNode.querySelector(".size_btn").remove("size_btn");
+		e.target.parentNode.querySelector(".rotate_btn").remove("rotate_btn");
 	}
 
 	Square.prototype.setParent = function(parent) {
